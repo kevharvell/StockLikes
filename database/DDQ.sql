c7c89c5ff028ce7e0109e097b3851afd013c332e
Kevin Harvell <harvellk@oregonstate.edu>
2018-10-26 21:13:49 -0500

    Changed SQL Fri, Oct 26, 2018  9:13:49 PM attribute to

--- database/DDQ.sql
+++ database/DDQ.sql
@@ -21,7 +21,7 @@ DROP TABLE IF EXISTS `stock`;
 CREATE TABLE `stock`(
 	`id` int(11) NOT NULL AUTO_INCREMENT,
 	`ticker` varchar(6) NOT NULL,
-	`date` date NOT NULL,
+	`date_recorded` date NOT NULL,
 	`price_close` DECIMAL(8, 2),
 	`companyID` int(11) NOT NULL,
 	PRIMARY KEY (`id`),
@@ -31,7 +31,7 @@ CREATE TABLE `stock`(
 DROP TABLE IF EXISTS `twitter`;
 CREATE TABLE `twitter` (
 	`id` int(11) NOT NULL AUTO_INCREMENT,
-	`date` date NOT NULL,
+	`date_recorded` date NOT NULL,
 	`url` varchar(80),
 	`buzz` int(20),
 	`companyID` int(11) NOT NULL,
@@ -67,8 +67,8 @@ CREATE TABLE `game_genre` (
 
 
 INSERT INTO `gaming_company` (comp_name) values ("Nintendo");
-INSERT INTO `stock` (ticker, date, price_close, companyID)v menalues ("NTDOY", "2018-10-5", 45.15, 1);
-INSERT INTO `twitter` (date, url, buzz, companyID) values ("2018-10-11", "https://twitter.com/NintendoAmerica", 120825, 1); 
+INSERT INTO `stock` (ticker, date_recorded, price_close, companyID)v menalues ("NTDOY", "2018-10-5", 45.15, 1);
+INSERT INTO `twitter` (date_recorded, url, buzz, companyID) values ("2018-10-11", "https://twitter.com/NintendoAmerica", 120825, 1); 
 INSERT INTO `game` (game_name, release_date, rating, companyID) values ("Super Mario Party", "2018-10-5", 2, 1);
 INSERT INTO `genre` (category) values ("Party");
 INSERT INTO `game_genre` (gameID, genreID) values (1, 1);
--- database/DMQ.sql
+++ database/DMQ.sql
@@ -10,10 +10,12 @@ Authors: Kevin Harvell and Will Darnell
 Last Update: 10/26/2018
 */
 
+--for all the id-related stuff, would that be this.id selection from the JS? in the sample file
+--all of it is referred to by "from dropdown menu" or something like that.
+
 
 --show all gaming companies
 SELECT comp_name FROM gaming_company;
-
 
 --show all stocks
 SELECT companyID, ticker, price_close FROM stock;
@@ -27,26 +29,37 @@ SELECT companyID, game_name FROM game;
 --show all genres
 SELECT category FROM genre;
 
---add a new gaming company
-INSERT INTO `gaming_company` (comp_name) values (:comp_name);
+--this is for the displaying Gaming Companies on Home
+SELECT gaming_company.comp_name, 
+stock.ticker, stock.price_close, twitter.url, twitter.buzz,
+game.game_name
 
+
+FROM (((gaming_company
+
+INNER JOIN stock ON gaming_company.id = stock.companyID)
+INNER JOIN twitter ON gaming_company.id = twitter.companyID)
+INNER JOIN game ON gaming_company.id = game.companyID);
+
+--this selects games with a high rating
+SELECT game_name, rating 
+
+FROM game WHERE game.rating >= 2;
+ 
+--add a new gaming company
+INSERT INTO gaming_company (comp_name) values (:comp_name);
 
 --add a new stock
-INSERT INTO `stock` (ticker, date, price_close, companyID) values (:ticker, :date, :price_close, :id_value);
-
+INSERT INTO stock (ticker, date, price_close, companyID) values (:ticker, :date, :price_close, :id_value);
 
 --add a new twitter page
-INSERT INTO `twitter` (date, url, buzz, companyID) values (:date, :url, :buzz, :id_value); 
-
-
+INSERT INTO twitter (date, url, buzz, companyID) values (:date, :url, :buzz, :id_value); 
 
 --add a new game 
-INSERT INTO `game` (game_name, release_date, rating, companyID) values (:gameName, :date, :rating, :companyID_value);
-
+INSERT INTO game (game_name, release_date, rating, companyID) values (:gameName, :date, :rating, :companyID_value);
 
 --add a new genre
-INSERT INTO `genre` (category) values (:category);
-
+INSERT INTO genre (category) values (:category);
 
 
 --edit gaming company
@@ -55,36 +68,27 @@ UPDATE gaming_company SET comp_name = :comp_name WHERE id = :gaming_company_ID_f
 --edit stock
 UPDATE stock SET ticker = :ticker_value, date = :date, price_close = :price_close, buzz = :buzz, WHERE id = :stock_ID_from_input;
 
-
-
 --edit twitter page
 UPDATE twitter SET date= :date, date = :date_value, url = :url, buzz = :buzz, WHERE id = :twitter_ID_from_input;
-
-
-
 
 --edit game
 UPDATE game SET game_name = :gameName, release_date = :date, rating = :rating WHERE id = :game_id_input;
 
 
-
 --delete gaming company
 DELETE FROM gaming_company WHERE id = :gaming_company_ID_from_selection;
-
-
-
 
 --delete stock
 DELETE FROM stock WHERE id = :stock_ID_from_selection;
 
-
 --delete twitter page
 DELETE FROM twitter WHERE id = :twitter_ID_from_selection;
-
 
 --delete game
 DELETE FROM game WHERE id = :game_name_selection;
 
-
 --delete genre
 DELETE FROM genre WHERE id = :genre_from_selection;
+
+--delete entry in game_genre table
+DELETE FROM game_genre WHERE gameID = :game_name_selection and genreID = :genre_from_selection;
