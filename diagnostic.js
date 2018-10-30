a22ed970afaed36726d50b37f2807fcb59775ca5
Kevin Harvell <harvellk@oregonstate.edu>
2018-10-30 00:31:45 -0500

    Finished insert and select for stocks page :)

--- diagnostic.js
+++ diagnostic.js
@@ -16,19 +16,15 @@ app.use(express.static(__dirname + '/public'))
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+// HOME PAGE
 app.get('/', function(req, res, next) {
   var context = {
     title: "Stock Likes Database"
   };
-	/*mysql.pool.query('INSERT INTO diagnostic (`text`) VALUES ("MySQL is Working!")', function(err) {
-	  mysql.pool.query('SELECT * FROM diagnostic', function(err, rows, fields){
-		  context.results = JSON.stringify(rows);
-		  res.render('home',context);
-		});
-	});*/
   res.render('home', context);
 });
 
+// ABOUT PAGE
 app.get('/about', function(req, res, next) {
   var context = {
     title: "Stock Likes - About"
@@ -36,25 +32,22 @@ app.get('/about', function(req, res, next) {
   res.render('about', context);
 });
 
+// GAMING COMPANIES PAGE - GET
 app.get('/gaming-companies', function(req, res, next) {
   var context = {
     title: "Stock Likes - Gaming Companies"
   };
   // Populate database to table
-  let sqlShow = "SELECT * FROM gaming_company"
+  let sqlShow = "SELECT comp_name FROM gaming_company";
   mysql.pool.query(sqlShow, function(err, rows, fields) {
     if(err) throw err;
-    console.log(rows);
     context.gaming_company = rows;
-    console.log(context.gaming_company);
     res.render('gaming-companies', context);
   });
 });
 
+// GAMING COMPANIES PAGE - POST
 app.post('/gaming-companies', function(req, res, next) {
-  var context = {
-    title: "Stock Likes - Gaming Companies"
-  };
   let sqlInsert = "INSERT INTO gaming_company (comp_name) VALUES (?)";
   let insertParams = [req.body.comp_nameInput];
 
@@ -63,24 +56,52 @@ app.post('/gaming-companies', function(req, res, next) {
     if(err) throw err;
     console.log("Number of records inserted: " + result.affectedRows);
     // Populate database to table
-    let sqlShow = "SELECT * FROM gaming_company"
-    mysql.pool.query(sqlShow, function(err, rows, fields) {
-      if(err) throw err;
-      console.log(rows);
-      context.gaming_company = JSON.stringify(rows);
-      console.log(context.gaming_company);
-      res.render('gaming-companies', context);
-    });
+    res.redirect('/gaming-companies');
   });
 });
 
+// STOCKS PAGE - GET
 app.get('/stocks', function(req, res, next) {
   var context = {
     title: "Stock Likes - Stocks"
   };
-  res.render('stocks', context);
+  // Populate database to table
+  // Populate Gaming Company Drop-down menu
+  let sqlShowComps = "SELECT id, comp_name FROM gaming_company";
+  mysql.pool.query(sqlShowComps, function(err, rows, fields) {
+    if(err) throw err;
+    context.gaming_company = rows;
+
+    //Populate Current Stocks Table
+    let sqlShowComps = "SELECT gaming_company.id, gaming_company.comp_name, stock.ticker, stock.date, stock.price_close " 
+                     + "FROM gaming_company "
+                     + "INNER JOIN stock ON gaming_company.id = stock.companyID";
+    mysql.pool.query(sqlShowComps, function(err, rows, fields) {
+      if(err) throw err;
+      context.stock = rows;
+      res.render('stocks', context);
+    });
+  });
 });
 
+// STOCKS PAGE - POST
+app.post('/stocks', function(req, res, next) {
+  var context = {
+    title: "Stock Likes - Stocks"
+  };
+  let sqlInsert = "INSERT INTO stock (ticker, date, price_close, companyID) VALUES (?, ?, ?, ?)";
+  let insertParams = [req.body.tickerInput, req.body.dateInput, req.body.price_closeInput, req.body.gaming_companyInput];
+
+  mysql.pool.query(sqlInsert, insertParams, function(err, result) {
+    // Show results of INSERT in console
+    if(err) throw err;
+    console.log("Number of records inserted: " + result.affectedRows);
+    // Populate database to table
+    res.redirect('/stocks');
+  });
+});
+
+// TWITTERS PAGE - GET
 app.get('/twitters', function(req, res, next) {
   var context = {
     title: "Stock Likes - Twitter Pages",
